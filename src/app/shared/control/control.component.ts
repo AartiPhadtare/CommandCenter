d8340ac0268746a241e1962ec1c9a9c2a710bf6d
Aarti <aarti@support.unlocktech.in>
2025-03-27 10:58:18 -0500

    commit

---
+++ src/app/shared/control/control.component.ts
@@ -0,0 +1,17 @@
+import { Component, input, Input, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-control',
+  imports: [],
+  templateUrl: './control.component.html',
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host:{
+    class: 'control'
+  }
+})
+export class ControlComponent {
+label = input<string>();
+
+
+}
